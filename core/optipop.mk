d0b54024fbb03051ac2d17db0dce61af53f97688
Chet Kener <Cl3Kener@gmail.com>
2015-03-21 08:51:50 -0400

    Update Floop Nest Optimize Module List
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/optipop.mk
+++ core/optipop.mk
@@ -293,7 +293,12 @@ ifeq ($(FLOOP_NEST_OPTIMIZE),true)
 LOCAL_ENABLE_NEST := \
 	art \
 	libsigchain \
+	libart \
+	libart-compiler \
+	libartd \
+	libartd-compiler \
 	libart-disassembler \
+	libartd-disassembler \
 	core.art-host \
 	core.art \
 	cpplint-art-phony \
@@ -322,7 +327,7 @@ LOCAL_ENABLE_NEST := \
 	libm \
 	tzdata \
 	bionic-benchmarks
-	
+
 ifeq (1,$(words $(filter $(LOCAL_ENABLE_NEST), $(LOCAL_MODULE))))
 ifdef LOCAL_CONLYFLAGS
 LOCAL_CONLYFLAGS += \
