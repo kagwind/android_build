df8529081de9903de6394d63417b2f5a212277bf
GWX <kagwind@gmail.com>
2015-05-20 22:28:49 +0800

    Remove unused apps.
    Change-Id: Ief54dbf724b6da78f947293104e18f07fe1df251

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -35,9 +35,6 @@ PRODUCT_LOCALES := en_US
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudioSlim.mk)
 
-# Get the TTS language packs
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
-
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
